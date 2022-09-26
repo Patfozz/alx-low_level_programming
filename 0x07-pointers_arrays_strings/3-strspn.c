df6d8110733789993f11eb26b2f96258d39ddc8f
Patfozz <patfoskypin@gmail.com>
2022-09-26 13:08:39 -0700

    strspn

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+* _strspn - a function to get the length of a string
+* @s : the string
+* @accept : when the length will be saved
+* Return: always return n
+*/
+
+unsigned int _strspn(char *s, char *accept)
+{
+unsigned int n;
+n = strspn(s, accept)
+return (n);
+}
