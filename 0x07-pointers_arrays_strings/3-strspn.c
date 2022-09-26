d7f890520daf8f53d5fe9fc130975a23d1964bbb
Patfozz <patfoskypin@gmail.com>
2022-09-26 13:14:49 -0700

    god abeg oo

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -10,7 +10,21 @@
 
 unsigned int _strspn(char *s, char *accept)
 {
-unsigned int n;
-n = strspn(s, accept)
-return (n);
+	unsigned int i, j, bool;
+
+	for (i = 0; *(s + i) != '\0'; i++)
+	{
+		bool = 1;
+		for (j = 0; *(accept + j) != '\0'; j++)
+		{
+			if (*(s + i) == *(accept + j))
+			{
+				bool = 0;
+				break;
+			}
+		}
+		if (bool == 1)
+			break;
+	}
+	return (i);
 }
