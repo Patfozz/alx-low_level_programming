faed8e73c0a220b48d8bc360be69333dc0cce7eb
Patfozz <patfoskypin@gmail.com>
2022-10-13 08:18:07 -0700

    corrections

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,15 +1,16 @@
 #include "3-calc.h"
 
 /**
-* main - Program that performs simple operations
-* @argc: argument count.
-* @argv: argument vector.
-* Return: Always 0.
+* main - program that perfroms simple operations
+* @argc: number of arguments
+* @argv: array of arguments
+* Return: Always 0 (Success)
 */
 int main(int argc, char *argv[])
 {
-	int a, b;
-	int (*operation)(int, int);
+	int arg1, arg2, result;
+	char o;
+	int (*func)(int, int);
 
 	if (argc != 4)
 	{
@@ -17,23 +18,27 @@ int main(int argc, char *argv[])
 		exit(98);
 	}
 
-	if (argv[2][1])
+	arg1 = atoi(argv[1]);
+	arg2 = atoi(argv[3]);
+
+	func = get_op_func(argv[2]);
+
+	if (!func)
 	{
 		printf("Error\n");
 		exit(99);
 	}
 
-	operation = get_op_func(argv[2]);
+	o = *argv[2];
 
-	if (operation == NULL)
+	if ((o == '/' || o == '%') && arg2 == 0)
 	{
 		printf("Error\n");
-		exit(99);
+		exit(100);
 	}
+	result = func(arg1, arg2);
 
-	a = atoi(argv[1]);
-	b = atoi(argv[3]);
+	printf("%d\n", result);
 
-	printf("%d\n", operation(a, b));
 	return (0);
 }
